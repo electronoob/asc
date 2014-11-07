2eb9efd74ec082f637534cb9a22eb4500e7e6c35
electronoob <git@electronoob.com>
2014-11-07 10:13:41 +0000

    jump and call checked for destination existence

--- asc.js
+++ asc.js
@@ -54,6 +54,9 @@ function ast_top() {
 			return null;
 		}
 	}
+        this.rewindIterator = function () {
+             this.childIterator = 0;
+        }
 }
 /* object literal */
 var optbl = {
@@ -62,9 +65,7 @@ var optbl = {
     ret:  { id: 0x03, ac: 0, at: null },
     jmp:  { id: 0x04, ac: 1, at: {0: 'label'} },
     call: { id: 0x05, ac: 1, at: {0: 'label'} },
-
     push: { id: 0x06, ac: 1, at: {0: 'dec|hex|register'} },
-
     pop:  { id: 0x07, ac: 1, at: {0: 'register'} },
     mov:  { id: 0x08, ac: 2, at: {0: 'register', 1: 'dec|hex|register'} },
     div:  { id: 0x09, ac: 2, at: {0: 'register', 1: 'dec|hex|register'} },
@@ -191,7 +192,6 @@ for (i in asc) {
                 node.coc = 0;
                 ast.children.push(node);
             }
-
             continue;
         }
         if (!operation.error) {
@@ -252,7 +252,7 @@ for (i in asc) {
 };
 
 /* check syntax and build ast */
-labels = Array(); // list labels for dupe check
+labels = Array(); // list labels for dupe check - used later also
 
 while (child = ast.childNext()) {
     param = child.param;
@@ -292,4 +292,41 @@ while (child = ast.childNext()) {
 /* without main we can't really do a lot */
 if (labels.indexOf('main') == -1) {
     spit(-1,-1,":main is missing!");
+}
+
+
+//console.log(ast);
+
+/* labels = Array(); <-- specified earlier */
+/* resolve jumps and calls to their destination */
+
+/*
+var optbl = {
+    inc:  { id: 0x01, ac: 0, at: null },
+    dec:  { id: 0x02, ac: 0, at: null },
+    ret:  { id: 0x03, ac: 0, at: null },
+    jmp:  { id: 0x04, ac: 1, at: {0: 'label'} },
+    call: { id: 0x05, ac: 1, at: {0: 'label'} },
+    push: { id: 0x06, ac: 1, at: {0: 'dec|hex|register'} },
+    pop:  { id: 0x07, ac: 1, at: {0: 'register'} },
+    mov:  { id: 0x08, ac: 2, at: {0: 'register', 1: 'dec|hex|register'} },
+    div:  { id: 0x09, ac: 2, at: {0: 'register', 1: 'dec|hex|register'} },
+    mul:  { id: 0x0A, ac: 2, at: {0: 'register', 1: 'dec|hex|register'} }
+};
+*/
+
+ast.rewindIterator();
+while (child = ast.childNext()) {
+    /* jmp  */
+    if(child.cmd == 0x04) {
+        if (labels.indexOf(child.param[0]) == -1) {
+            spit(child.loc,child.coc,"Destination label :"+child.param+" is missing.");
+        }
+    }
+    /* call  */
+    if(child.cmd == 0x05) {
+        if (labels.indexOf(child.param[0]) == -1) {
+            spit(child.loc,child.coc,"Destination label :"+child.param+" is missing.");
+        }
+    }
 }
